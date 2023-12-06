5b736b1cc827bd03a7d133850f1e2e2f66f1e4ed
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-06 23:22:44 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -6,9 +6,24 @@ st.set_page_config(page_title='Homepage', page_icon=':books:', layout='wide')
 st.sidebar.success('Please select a page from above')
 
 with st.container():
-    st.title('Home')
+    rain(
+        emoji='❄',
+        font_size=16,
+        falling_speed=5,
+        animation_length='infinite',
+    )
+
+with st.container():
+    col1, col2, col3 = st.columns(3)
+    st.subheader('🏠 Home')
     st.write('---')
-    st.subheader('Welcome to my website, for now it will only contain two pagese consisting of this and the calculator.')
+    st.write('##')
+    with col1:
+        st.write('')
+    with col2:
+        st.subheader('Welcome to my website, for now it will only contain two pages consisting of this and the calculator.')
+    with col3:
+        st.write('')
     st.write('##')
     st.write('---')
 
@@ -18,10 +33,4 @@ with st.container():
         color_name='violet-70',
     )
 
-with st.container():
-    rain(
-        emoji='❄',
-        font_size=16,
-        falling_speed=5,
-        animation_length='infinite',
-    )
+
