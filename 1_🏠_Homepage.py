95ecbabd94894820689da18d17b9cd058cf7eb3c
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-06 20:08:59 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -1,21 +1,36 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
+
 st.set_page_config(page_title='Homepage', page_icon=':books:', layout='wide')
 st.sidebar.success('Please select a page from above')
 
 with st.container():
-    st.title("Home")
+    st.title('Home')
     st.write('---')
-    st.subheader('Welcome to my website, for now it will only contain two pages consisting of this and the calculator. It will eventually increase in the future.')
+    st.subheader('Welcome to my website, for now it will only contain two pagese consisting of this and the calculator.')
     st.write('##')
     st.write('---')
 
 colored_header(
     label='Contents',
-    color(violet-70)
+    color_name='violet-70'
 )
+import streamlit as st
+from streamlit_extras.colored_header import colored_header
 
 
+st.set_page_config(page_title='Homepage', page_icon=':books:', layout='wide')
+st.sidebar.success('Please select a page from above')
 
+with st.container():
+    st.title('Home')
+    st.write('---')
+    st.subheader('Welcome to my website, for now it will only contain two pagese consisting of this and the calculator.')
+    st.write('##')
+    st.write('---')
 
+colored_header(
+    label='Contents',
+    color_name='violet-70'
+)
