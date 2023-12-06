e0d23352b30051a5de27e44ca269ef5cb57c579f
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-06 19:57:34 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -1,16 +1,13 @@
 import streamlit as st
-from PIL import Image
 
-st.set_page_config(page_title='General Mathematics', page_icon=':books:', layout='wide')
 
-img_r_formula = Image.open('images/r_formula.png')
-img_n_formula = Image.open('images/n_formula.png')
+st.set_page_config(page_title='Homepage', page_icon=':books:', layout='wide')
 st.sidebar.success('Please select a page from above')
 
 with st.container():
-    st.title("General Mathematics: Future Value of General Annuity Calculator")
+    st.title("Home")
     st.write('---')
-    st.subheader('This will be used to calculate the Future values for the performance task in General Mathematics')
+    st.subheader('Welcome to my website, for now it will only contain two pages consisting of this and the calculator. It will eventually increase in the future')
     st.write('##')
 
 
