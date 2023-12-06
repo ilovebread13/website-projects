052a005ab6708b6ed9070f653ef657227934ff0c
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-06 19:59:24 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -7,9 +7,13 @@ st.sidebar.success('Please select a page from above')
 with st.container():
     st.title("Home")
     st.write('---')
-    st.subheader('Welcome to my website, for now it will only contain two pages consisting of this and the calculator. It will eventually increase in the future')
+    st.subheader('Welcome to my website, for now it will only contain two pages consisting of this and the calculator. It will eventually increase in the future.')
     st.write('##')
+    st.write('---')
 
+with st.container():
+    st.subheader('Contents')
+    st.write('---')
 
 
 
