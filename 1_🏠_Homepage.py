ddaa88d5f9957d04585654033c9c332fc4078263
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-06 23:37:13 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -31,7 +31,7 @@ with st.container():
     )
     switch_pages = st.button('Go to Calculator!')
     if switch_pages:
-        switch_page('2_🧮_Calculator.py')
+        switch_page('2_🧮_Calculator')
 
     
 
