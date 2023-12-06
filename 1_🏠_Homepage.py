0ef87128c858b7baf76b8530426052f5b0817c06
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-06 20:31:25 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_extras.colored_header import colored_header
+
 from streamlit_extras.let_it_rain import rain
 
 st.set_page_config(page_title='Homepage', page_icon=':books:', layout='wide')
@@ -12,11 +12,7 @@ with st.container():
     st.write('##')
     st.write('---')
 
-with st.container():
-    colored_header(
-        label='Contents',
-        color_name='violet-70',
-    )
+
 
 with st.container():
     rain(
