c31496ddcc0108cc8bb069eecbf9b61314955701
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-07 15:04:32 +0800

    Update 3_🪨_Rocks.py

--- pages/3_🪨_Rocks.py
+++ pages/3_🪨_Rocks.py
@@ -7,6 +7,13 @@ from streamlit_extras.switch_page_button import switch_page
 img_rocks = Image.open('images/rocks.png')
 
 with st.container():
+  st.title('15 interesting rocks from National Geographic')
+  st.write('##')
+  switch_pages = st.button('Go back to homepage')
+  if switch_pages:
+    switch_page('homepage')
+
+with st.container():
   col1, col2, col3 = st.columns(3)
   col1 = st.write('##')
   col2 = st.image(img_rocks)
