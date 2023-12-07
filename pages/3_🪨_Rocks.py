cb6ee9b916256876fe96b6aa470f41d0820b9997
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-07 15:08:50 +0800

    Update 3_🪨_Rocks.py

--- pages/3_🪨_Rocks.py
+++ pages/3_🪨_Rocks.py
@@ -9,12 +9,13 @@ img_rocks = Image.open('images/rocks.png')
 with st.container():
   st.title('15 interesting rocks from National Geographic')
   st.write('##')
-  switch_pages = st.button('Go back to homepage')
-  if switch_pages:
-    switch_page('homepage')
+  
 
 with st.container():
   col1, col2, col3 = st.columns(3)
   col1 = st.write('##')
   col2 = st.image(img_rocks)
   col3 = st.write('##')
+  switch_pages = st.button('Go back to homepage')
+  if switch_pages:
+    switch_page('homepage')
