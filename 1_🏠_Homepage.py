a4e611d7655689e5b1c5ed2a0b4f2a2fbf4e963e
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-07 19:30:00 +0800

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -48,8 +48,8 @@ with st.container():
 
     left_column, right_column = st.columns((2, 1))
     with left_column:
-        st.subheader('Welcome to our site!')
-        st.write('There will be more features in the future, but for now we only have a few')
+        st.subheader('❤ Welcome to our site! ❤')
+        st.write('• There will be more features in the future, but for now we only have a few.')
     with right_column:
         st.lottie(lottie_animation, height=350)
     st.write('##')
