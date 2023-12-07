bfe7db76e0a029a7643b3d1f656cbe5ace142cbc
ilovebread13 <153002365+ilovebread13@users.noreply.github.com>
2023-12-07 18:31:37 +0800

    Update 3_💎_Rocks.py

--- pages/3_💎_Rocks.py
+++ pages/3_💎_Rocks.py
@@ -21,7 +21,7 @@ with st.container():
   
   
 with st.container():
-  col1, col2, col3 = st.columns((1, 1, 1))
+  col1, col2, col3 = st.columns(3)
   with col1 = st.write('##')
   with col2 = st.image(img_rocks)
   with col3 = st.write('##')
